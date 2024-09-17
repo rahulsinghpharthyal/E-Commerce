b230f05917a37cc45ccf3b299d49c3cbea7425b5
Rahul_Pharthyal_MNNLR <mern.mnnlr.rahulpharthyal@gmail.com>
2024-09-17 20:12:26 +0530

    chang ebaceend url

--- src/customAxios/privateAxios.js
+++ src/customAxios/privateAxios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://ecommerce-server-m7b4.onrender.com";
+
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
