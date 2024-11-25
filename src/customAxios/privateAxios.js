a2d43a3d50c9a542f505644bdc06d9de81dbe4a1
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-11-25 19:55:47 +0530

    change backend url

--- src/customAxios/privateAxios.js
+++ src/customAxios/privateAxios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-// const BASE_URL = "http://localhost:8000";
-const BASE_URL = "https://ecommerce-server-m7b4.onrender.com";
+const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "https://ecommerce-server-m7b4.onrender.com";
 
 
 export const axiosPrivate = axios.create({
